6760d705e11ebc5cd49d526b6544968fa9e14a57
Savvy_Through <savithruml@users.noreply.github.com>
2016-02-11 12:54:08 -0700

    Create wordCountRun.sh

---
+++ wordCountRun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+chmod a+x /Users/Savithru/Desktop/src/wordcount_median.py
+
+python /Users/Savithru/Desktop/src/wordcount_median.py /Users/Savithru/Desktop/wc_input /Users/Savithru/Desktop/wc_output/wc_result.txt
+python /Users/Savithru/Desktop/src/wordcount_median.py /Users/Savithru/Desktop/wc_input /Users/Savithru/Desktop/wc_output/med_result.txt
